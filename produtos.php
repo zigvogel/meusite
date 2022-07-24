3ac505f1f61feccec5a5e59bd17b3a7c541ed01c
zigvogel <92961398+zigvogel@users.noreply.github.com>
2022-07-23 21:17:56 -0300

    meusite

---
+++ produtos.php
@@ -0,0 +1,12 @@
+<?php
+// include do footer
+include_once './includes/_banco.php';
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+?>
+<h1>Produtos</h1>
+
+<?php
+// include do footer
+include_once './includes/_footer.php';
+?>
